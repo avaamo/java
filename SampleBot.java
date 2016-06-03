469102e7b68a83a7fd1cfa068bf162f44d4a2977
Dheeraj <spddheeraj@gmail.com>
2016-06-03 14:48:56 +0530

    sample files added and Sample bot updated

--- SampleBot.java
+++ SampleBot.java
@@ -1,19 +1,23 @@
-package com.avaamo.java.test;
 
 import java.io.File;
 import com.avaamo.java.Avaamo;
 import com.avaamo.java.attachments.CardAttachment;
 import com.avaamo.java.attachments.FileAttachment;
 import com.avaamo.java.attachments.Image;
+import com.avaamo.java.models.Activity;
 import com.avaamo.java.models.IncomingMessageModel;
 import com.avaamo.java.models.ReadAckModel;
+import com.avaamo.java.models.IncomingMessageModel.FileAttachmentResponse;
 
 public class SampleBot {
 
 	static Avaamo avaamo;
 
-	static String bot_uuid = "2aa5c062-4aba-40e5-9200-e853ca5ed4da";
-	static String access_token = "eXatcc2s1pZUty7vD48BzvZQtmIJ8jP7";
+	static String bot_uuid = "0f34af86-3310-4b0d-aeac-3b9e7eab84f2";
+	static String access_token = "m1Mvdhj4HF3awFQ2InVTpSJ1tGy5efba";
+	
+//	static String bot_uuid = "1b1fa0ef-4f01-47d8-9959-d62a5fac2593";
+//	static String access_token = "tVq-9ceBf0laP7S0CThH9jnc4QR6PQzG";
 
 	public static void main(String[] args) throws Exception {
 
@@ -35,23 +39,29 @@ public class SampleBot {
 					System.out.println("\n==> "+message.user.firstName+": "+ content);					
 					String cuuid = message.conversation.uuid;
 					
-
+					avaamo.readMessage(message);
 					
-					switch(content.toUpperCase()){
+					if(message.message.attachment instanceof FileAttachmentResponse){
+						FileAttachmentResponse fileAttachmentResponse = (FileAttachmentResponse) message.message.attachment;
+						avaamo.downloadAttachment(fileAttachmentResponse.uuid, new File("download"));
+					}
+
+					if(content != null){
+						switch(content.toUpperCase()){
 						case "IMAGE":
 							// Image example
 							Image image = new Image(new File("test_image.jpg"));
 							image.setCaption("This is the image caption");
 							avaamo.sendImage(image, cuuid);
 							break;
-							
+
 						case "FILE":
 							// File example
 							FileAttachment fileAttachment = new FileAttachment(new File("TestFile.txt"));
 							avaamo.sendFileAttachment(fileAttachment, cuuid);
 							break;
 						case "CARD":
-							
+
 							// Card example
 							CardAttachment cardAttachment = new CardAttachment();
 							cardAttachment.setTitle("Card Title");
@@ -67,27 +77,33 @@ public class SampleBot {
 							// Sample action response
 							avaamo.sendMessage("Sample action response.", cuuid);
 							break;
-							
-							
+
+
 						default:
 							// Default welcome message
 							avaamo.sendMessage("Awesome. It works!. \nType one of the following to see them in action. \nimage \nfile \ncard", cuuid);
 							break;
+						}
 					}
-					
-					
+					else{
+						avaamo.sendMessage("Awesome. It works!. \nType one of the following to see them in action. \nimage \nfile \ncard", cuuid);
+					}
 
-					
 				}catch (Exception error){
 					error.printStackTrace();
 					System.err.println("Error processing the message."+ error.getMessage());
 				}
 
 			}
-			
+
 			@Override
 			public void handleReadAck(ReadAckModel readAckModel) {
 				System.out.println("Incoming read ack for message uuid : " + readAckModel.read_ack.message_uuid );
+			}
+			
+			@Override
+			public void handleActivity(Activity activity) {
+				System.out.println("Incoming activity from user : " + activity.user.firstName + " " + activity.user.lastName );
 			}
 		});
 
