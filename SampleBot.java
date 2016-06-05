f4760ae4eb5610388d53e2c4b9fac4c78e4b69b2
Dheeraj <spddheeraj@gmail.com>
2016-06-05 09:49:28 +0530

    updated sample and avaamo jar for handling form response

--- SampleBot.java
+++ SampleBot.java
@@ -1,13 +1,22 @@
 
 import java.io.File;
+
 import com.avaamo.java.Avaamo;
 import com.avaamo.java.attachments.CardAttachment;
 import com.avaamo.java.attachments.FileAttachment;
 import com.avaamo.java.attachments.Image;
 import com.avaamo.java.models.Activity;
+import com.avaamo.java.models.FormAttachmentResponse;
 import com.avaamo.java.models.IncomingMessageModel;
-import com.avaamo.java.models.ReadAckModel;
 import com.avaamo.java.models.IncomingMessageModel.FileAttachmentResponse;
+import com.avaamo.java.models.ReadAckModel;
+import com.avaamo.java.models.form.FormResponse;
+import com.avaamo.java.models.form.Question;
+import com.avaamo.java.models.form.ReplyData.CheckListData;
+import com.avaamo.java.models.form.ReplyData.FileData;
+import com.avaamo.java.models.form.ReplyData.ImageGroupData;
+import com.avaamo.java.models.form.ReplyData.LocationData;
+import com.avaamo.java.models.form.ReplyData.OptionData;
 
 public class SampleBot {
 
@@ -44,6 +53,62 @@ public class SampleBot {
 					if(message.message.attachment instanceof FileAttachmentResponse){
 						FileAttachmentResponse fileAttachmentResponse = (FileAttachmentResponse) message.message.attachment;
 						avaamo.downloadAttachment(fileAttachmentResponse.uuid, new File("download"));
+					}
+					
+					else if(message.message.attachment instanceof FormAttachmentResponse){
+						System.out.println("Incoming form from user: ");
+						FormAttachmentResponse formAttachmentResponse = (FormAttachmentResponse) message.message.attachment;
+						System.out.println("Downloading form...");
+						FormResponse formResponse = formAttachmentResponse.downloadResponse();
+						System.out.println("Downloading form complete.");
+						for(Question question : formResponse.questions){
+							System.out.println(question.title);
+							System.out.println(question.reply.getAnswer());
+							switch (question.questionType) {
+							case AUTO_CHECKIN:
+							case LOCATION:
+								if(question.reply.replyData instanceof LocationData){
+									LocationData locationData = (LocationData) question.reply.replyData;
+									System.out.println("Location details: " + locationData.lat + " :: " + locationData.lng);
+								}
+								break;
+							case CHECKLIST:
+								if(question.reply.replyData instanceof CheckListData){
+									CheckListData checkListData = (CheckListData) question.reply.replyData;
+									System.out.println("selected option uuids");
+									for(String optionUUID : checkListData.option_uuid){
+										System.out.println(optionUUID);
+									}
+								}
+								break;
+							case FILE:
+							case IMAGE:
+							case SIGNATURE:
+								if(question.reply.replyData instanceof FileData){
+									FileData fileData = (FileData) question.reply.replyData;
+									fileData.assetInfo[0].downloadAttachment(new File(fileData.assetInfo[0].file_name));
+								}
+								break;
+							case IMAGE_GROUP:
+								if(question.reply.replyData instanceof ImageGroupData){
+									ImageGroupData imageGroupData = (ImageGroupData) question.reply.replyData;
+									System.out.println("Number of images attached: " + imageGroupData.fileNames.length);
+								}
+								break;
+							
+							case PICKLIST:
+							case POLL:
+								if(question.reply.replyData instanceof OptionData){
+									OptionData optionData = (OptionData) question.reply.replyData;
+									System.out.println("selected option uuid : "+ optionData.option_uuid);
+									
+								}
+								break;
+							}
+							System.out.println();
+						}
+						
+						
 					}
 
 					if(content != null){
